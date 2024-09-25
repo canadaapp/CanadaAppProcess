82ab02b5875cb2b75471e0059cf20fe3d4c8cbdd
canadaapp <olivialiam09876@gmail.com>
2024-09-26 00:19:14 +0600

    just make it

---
+++ app.js
@@ -0,0 +1,25 @@
+        
+        function redirect(){
+            var test =  document.querySelector('#passport');
+            if(test.value == "123456"){
+                    window.location.href="blank.html";
+            }else if(test.value == "EM0465896"){
+               window.location.href="rostom.html"
+            }else if(test.value== "A04970011"){
+                window.location.href="roman.html"
+            }else if(test.value== "A06328866"){
+                window.location.href="alamin.html"
+            }else if(test.value=="A06328866"){
+                window.location.href="alamin.html"
+            }
+            else{
+                 alert("invalid Passport")
+            }
+   
+              }
+           
+               
+             
+           
+         
+          
