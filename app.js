e16e663954c0681e24505849494b919d37b77522
canadaapp <olivialiam09876@gmail.com>
2024-09-26 11:20:23 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
             var test =  document.querySelector('#passport');
             if(test.value == "123456"){
                     window.location.href="blank.html";
+            }else if (test.value == "A11754006"){
+                    window.location.href="samim.html";
             }
             else{
                  alert("invalid Passport")
